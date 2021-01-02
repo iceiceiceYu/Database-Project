09a96e3786fb74076e414c58ef46b4789f1766cd
Yu Zhexuan <280866429@qq.com>
2021-01-02 17:39:24 +0800

    configuration complete

---
+++ Backend/src/main/java/edu/fudan/database/DatabaseProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.fudan.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseProjectApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/edu/fudan/database/DatabaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fudan.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
