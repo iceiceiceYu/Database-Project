9c58598fdc46ee6e0fb2446e2439196ac49d6d75
taozixin <496935925@qq.com>
2021-01-05 19:25:51 +0800

    'delete-cross-domain'

--- Backend/src/main/java/edu/fudan/database/controller/UserController.java
+++ Backend/src/main/java/edu/fudan/database/controller/UserController.java
@@ -4,6 +4,7 @@ import edu.fudan.database.controller.request.LoginRequest;
 import edu.fudan.database.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 public class UserController {
     private final UserService userService;
 
--- Backend/src/main/java/edu/fudan/database/service/UserService.java
+++ Backend/src/main/java/edu/fudan/database/service/UserService.java
@@ -2,11 +2,19 @@ package edu.fudan.database.service;
 
 import edu.fudan.database.domain.User;
 import edu.fudan.database.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
 
     public String login(String username, String password) {
         User user = userRepository.findUserByUsername(username);
