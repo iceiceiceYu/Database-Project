df1fd51c5c7e8bca222792e9ced5405a4c31a0e8
Yu Zhexuan <280866429@qq.com>
2021-01-06 15:17:09 +0800

    debug

--- Backend/src/main/java/edu/fudan/database/repository/ReportRepository.java
+++ Backend/src/main/java/edu/fudan/database/repository/ReportRepository.java
@@ -4,9 +4,9 @@ import edu.fudan.database.domain.Report;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ReportRepository extends CrudRepository<Report, Long> {
-    Iterable<Report> findReportByName(String name);
+    Iterable<Report> findReportByPatient(String patient);
 
     Iterable<Report> findReportByDate(String date);
 
-    Iterable<Report> findReportByNameAndDate(String name, String date);
+    Iterable<Report> findReportByPatientAndDate(String patient, String date);
 }
